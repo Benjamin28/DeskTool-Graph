25706b4fd3bd47d5ea4342062711b16dcf962ad9
benjamin28 <benjamin.baker@utexas.edu>
2016-12-23 14:19:31 -0600

    now shaders are compiled

--- canvas.js
+++ canvas.js
@@ -8,6 +8,43 @@ var verticies = [-0.5, 0.5, -0.5, -0.5, 0.0, -0.5,];
 
 var vertex_buffer = gl.createBuffer();
 
-gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
+gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer); //binding
 
 gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(verticies), gl.STATIC_DRAW);
+
+gl.bindBuffer(gl.ARRAY_BUFFER, null); //unbinding
+
+
+//set up shaders
+
+var vertCode =
+    'attribute vec2 coordinates;' +
+    'void main(void) {' + ' gl_Position = vec4(coordinates,0.0, 1.0);' + '}';
+
+var vertShader = gl.createShader(gl.VERTEX_SHADER);
+
+gl.shaderSource(vertShader, vertCode);
+gl.compileShader(vertShader);
+
+
+var fragCode = 'void main(void){' + 'gl_FragColor = vec4(0.0, 0.0, 0.0, 0.1);' + '}';
+
+var fragShader = gl.createShader(gl.FRAGMENT_SHADER);
+
+gl.shaderSource(fragShader, fragCode);
+gl.compileShader(fragShader);
+
+//now both shaders are compiled
+
+var shaderProgram = gl.createProgram();
+
+gl.attachShader(shaderProgram, vertShader);
+gl.attachShader(shaderProgram, fragShader);
+
+gl.linkProgram(shaderProgram);
+
+gl.useProgram(shaderProgram);
+
+//associate shader programs w/ buffer objects
+
+gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
