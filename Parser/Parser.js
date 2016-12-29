374711f0215154c1cab9215f1e82b2dcd93b2439
benjamin28 <benjamin.baker@utexas.edu>
2016-12-29 17:24:32 -0600

    major landmark in skeleton

--- Parser/Parser.js
+++ Parser/Parser.js
@@ -1,8 +1,81 @@
 // cursor advances from 0 to func_string.length()
 var cursor = 0;
-var values = [];
+var plot_coordinates = [];
 var function_string;
 
+function parser_high_priority(){
+
+
+    var indicator = function_string[cursor];
+    var result_list;
+
+    //init list to catch output
+    if(indicator == '(' || indicator == 'x' || is_int()){
+	result_list = new Array(plot_coordinates.length());
+    }else
+	print("ERROR, invalid function");
+
+    if(indicator == 'x'){
+
+	return_list = [for (x of plot_coordinates) x];
+	parser_increment_cursor();
+	return return_list;
+
+    }
+    if(is_num()){
+
+	return_list = parser_get_num();
+	//possible optimization, is_num_increment
+	parser_increment_cursor();
+	return return_list;
+	
+    }
+    if(indicator == '('){
+	print("ERROR: parenthesis not yet implemented");
+    }
+    
+}
+
+function parser_med_priority(){
+
+    //array
+    var hi_prio_left = parser_high_priority();
+    while(func_string[cursor] == '*'){
+
+	parser_increment_cursor();
+
+	//array
+	var hi_prio_right = parser_high_proiority();
+	var i = 0;
+	for (i; i < plot_coordinates.length(); i ++){
+	    hi_prio_left[i] = hi_prio_left[i] * hi_prio_right[i];
+	}
+	
+    }
+    return hi_prio_left;
+}
+
+
+function parser_low_priority(values){
+
+    //array
+    var med_prio_left = parser_med_priority();
+
+    while(func_string[cursor] == '+'){
+	
+	parser_increment_cursor();
+
+	//array
+	var med_prio_right = parser_med_priority();
+	var i = 0;
+	for(i; i < values.length(); i++){
+
+	    values[i] = med_prio_left[i] + med_prio_right[i];
+	    
+	}
+    }
+    return values;
+}
 
 
 /*The parser_plot function plots the function over the plot coordinates
@@ -11,14 +84,9 @@ var function_string;
   RETURNS: array of values that correspond to each value in plot_coords
 */
 
-function parser_expression(func_string, plot_vars){
+function parser_expression(values){
 
-    var left_hand = parser_low_priority(func_string, plot_vars);
-    while(func_string[cursor] == '+'){
-
-	
-	
-    }
+    return parser_low_priority(values);
 
 }
 
@@ -26,33 +94,16 @@ function parser_expression(func_string, plot_vars){
 function parser_plot(plot_coords, func_string){
 
     function_string = func_string;
+    plot_coordinates = plot_coords;
     
     //init values array
-    var i = 0;
-    for(i = 0; i < plot_coords.length; i++){
-	values.push(0);	
-    }
+    var values = new Array(plot_coords.length());
 
-    //set every var in values array to correct first value
-    for(i = 0; i < plot_coords.length; i++){
-
-	if(is_x()){
-	    values[i] = plot_coords[i];
-	}
-	else if(is_num){
-	    values[i] = parser_get_num()
-	}
-	else{
-	    print("ERROR: first char in expression invalid");
-	}
-    }
-    //increment cursor
-    parser_increment_cursor();
     
-    while(parser_expression(func_string, plot_coords));
+    parser_expression();
 
     print("done parsing");
-    print(plot_coords);
+    print(plot_coords, plot_vars);
     
     
 }
